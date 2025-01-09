3a6f410001f1c7a1362c103317f469c96bd335dd
Deepti <moogideepti5@gmail.co>
2025-01-09 22:15:28 +0530

    9th jan changed-added comment program

---
+++ src/JavaPrograms/CommentProgram.java
@@ -0,0 +1,10 @@
+package JavaPrograms;
+
+public class CommentProgram {
+    public static void main(String[] args)
+    {
+    System.out.println("Multiline comment -/* hello*/");
+        System.out.println("Single line comment - comment //");
+        System.out.println("Good Commenting");
+    }
+}
