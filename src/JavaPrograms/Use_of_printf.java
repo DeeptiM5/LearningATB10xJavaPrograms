6166e791a9e67d964234fff1fa9e08b7811c280e
Deepti <moogideepti5@gmail.co>
2025-01-11 22:18:02 +0530

    9th jan changed-added comment program

---
+++ src/JavaPrograms/Use_of_printf.java
@@ -0,0 +1,30 @@
+package JavaPrograms;
+
+public class Use_of_printf
+{
+    public static void main(String[] args) {
+         int a=9;
+
+
+        System.out.printf("9*1 = %d",a);
+        System.out.println();
+        System.out.printf("9*2 = %d",a*2);
+        System.out.println();
+        System.out.printf("9*3 = %d",a*3);
+        System.out.println();
+        System.out.printf("9*4 = %d",a*4);
+        System.out.println();
+        System.out.printf("9*5 = %d",a*5);
+        System.out.println();
+        System.out.printf("9*6 = %d",a*6);
+        System.out.println();
+        System.out.printf("9*7 = %d",a*7);
+        System.out.println();
+        System.out.printf("9*8 = %d",a*8);
+        System.out.println();
+        System.out.printf("9*9 = %d",a*9);
+        System.out.println();
+        System.out.printf("9*10= %d",a*10);
+
+    }
+}
